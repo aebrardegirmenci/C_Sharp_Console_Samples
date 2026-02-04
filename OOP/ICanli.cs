b28fd0676cf4c22fa11209e904e3b20c4ef3cbf9
aebrardegirmenci <aebrardegirmenci@gmail.com>
2026-02-04 21:25:00 +0300

    040226

---
+++ OOP/ICanli.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using OOP.Enums;
+// using HesapMakinesi; //References'tan eklenmeli.
+
+namespace OOP
+{
+    internal interface ICanli
+    {
+      //  HesapMakinesi.Program // yukarıa using yazarsak buna gerek kalmıyor.
+
+        string Name { get; set; }
+        Enums.Cinsiyet Tur { get; set; }
+
+        string Beslen();
+        string Ure();
+        string Dead();
+    }
+}
---
+++ OOP/Interfaces/IUçabilir.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces
+{
+    internal interface IUçabilir
+    {
+        string Uc();
+    }
+}

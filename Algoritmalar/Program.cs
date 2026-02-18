b8b687081c7481ff70d700dc0d6a563092123e8c
aebrardegirmenci <aebrardegirmenci@gmail.com>
2026-02-18 19:09:10 +0300

    Add files via upload

---
+++ Algoritmalar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmalar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+            Console.WriteLine("----------------------");
+
+            var Toplam = 5 + 8;
+
+            Console.WriteLine("Toplam: " + Toplam);
+            Console.WriteLine("----------------------");
+            string kullaniciAd, durum;
+            
+            kullaniciAd = "Vedat ";
+
+            Console.Write("Kullanıcı adınız: ");
+            kullaniciAd = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldin " + kullaniciAd);
+            Console.Write("Nasılsın? İyi misin?");
+
+            durum = Console.ReadLine();
+
+            Console.WriteLine(durum + " yazdınız..");
+            Console.WriteLine("----------------------");
+
+
+            Console.ReadKey();
+
+                
+        }
+    }
+}
